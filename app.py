b3c628d8c8d5037a1f27d05b1d09cd27f2e4b9e6
Renato_snp <renatoguitarbluest@gmail.com>
2020-07-13 19:14:39 -0300

    Melhorando index

--- app.py
+++ app.py
@@ -7,16 +7,11 @@ def index():
     return render_template("index.html"), 200
 
 
-@app.route('/login/')
-def login():
-    return render_template('login.html')
-
 @app.route('/cadastrar/')
 def cadastrar():
-    return render_template('cadastro.html')
+    return render_template("cadastro.html"), 200
 
         
-    
 
 if __name__ == "__main__":
     app.run(debug=True)
